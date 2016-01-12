edc419fb77eabab45b2afb13b111365106913154
ipapapa <iduckhd@hotmail.com>
2016-01-11 17:49:27 -0800

    reconciliation v1 entropy files

---
+++ src/entropy/dyn_entropy_snd.c
@@ -0,0 +1,353 @@
+/*
+ * Dynomite - A thin, distributed replication layer for multi non-distributed storages.
+ * Copyright (C) 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *_stats_pool_set_ts
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> // for open
+#include <unistd.h> //for close
+#include <math.h> // to do ceil for number of chunks
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/sendfile.h>
+#include <netinet/in.h>
+
+#include "dyn_core.h"
+
+/*
+ * Function:  compact_aof
+ * --------------------
+ *
+ * Performs background Redis rewrite of aof.
+ * It tries to rewrite the aof twice before sending to Spark.
+ * If the second time fails, the Socket to spark is closed.
+ */
+
+static rstatus_t
+compact_aof(){
+	char 			command[BUFFER_SIZE];
+    int 			sys_ret = 0;
+
+	memset(&command[0], 0, sizeof(command));
+    sprintf(command, "redis-cli -p 22122 bgrewriteaof");
+    sys_ret = system(command);
+    if( sys_ret < 0 ){
+	    log_error("Error on system call --> %s", strerror(errno));
+	    loga("Thread sleeping 2 seconds and retrying");
+	    sleep(2);
+	    sys_ret = system(command);
+	    if( sys_ret < 0 ){
+		    log_error("Error on bgrewriteaof for seconds time --> %s", strerror(errno));
+			return DN_ERROR;
+	    }
+    }
+    loga("Redis BGREWRITEAOF completed");
+    return DN_OK;
+}
+
+
+/*
+ * Function:  entropy_snd_callback
+ * --------------------
+ *
+ * Handling connection for each client on a separate thread
+ */
+
+static void
+entropy_snd_callback(void *arg1, void *arg2)
+{
+    struct stat     file_stat;
+    ssize_t         transmit_len;
+    int peer_socket;
+    FILE			*fp = NULL;
+    int             fd;
+    char            buff[BUFFER_SIZE];
+    unsigned char ciphertext[CIPHER_SIZE];
+    int ciphertext_len = 0;
+    size_t 			nread;
+    int				nchunk;
+    int 			i; //iterator for chunks
+    size_t 			last_chunk_size;
+    int n = *((int *)arg2);
+
+    struct entropy *st = arg1;
+
+
+    /* check for issues */
+    if (n == 0 || CIPHER_SIZE < BUFFER_SIZE) {
+  //  	log_error("cipher or header size are bigger than buffer size");
+  //  	log_error("cipher size: %d -  buffer size: %d - header_size: %d", CIPHER_SIZE, BUFFER_SIZE, HEADER_SIZE);
+   	    return;
+    }
+
+    if(ENCRYPT_FLAG == 0){
+    	loga("Encryption is disabled for reconciliation");
+    }
+
+    /* Initialize the crypto library */
+    ERR_load_crypto_strings();
+    OpenSSL_add_all_algorithms();
+    OPENSSL_config(NULL);
+
+    /* accept the connection */
+    peer_socket = accept(st->sd, NULL, NULL);
+    if(peer_socket < 0){
+    	log_error("peer socket coould not be established");
+    	goto error;
+    }
+    loga("Spark socket connection accepted"); //TODO: print information about the socket IP address.
+
+    /* compact AOF in Redis before sending to Spark */
+    if(compact_aof() == DN_ERROR){
+    	log_error("Redis failed to perform bgrewriteaof");
+    	goto error;
+    }
+
+    /* create a file pointer for the AOF */
+    fp = fopen(AOF_TO_SEND, "r");
+    if (fp == NULL)
+    {
+    	log_error("Error opening Redis AOF file: %s", strerror(errno));
+    	goto error;
+    }
+    loga("Redis AOF loaded successfully");
+
+    /* Get the file descriptor from the file pointer */
+    fd = fileno(fp);
+
+    /* Get the file size to include in the header */
+    if (fstat(fd, &file_stat) < 0)
+    {
+    	 log_error("Error fstat --> %s", strerror(errno));
+     	 goto error;
+    }
+
+    /* clear buffer before using it */
+    memset(&buff[0], 0, sizeof(buff));
+    buff[0] = (int)((((int)file_stat.st_size) >> 24) & 0xFF);
+    buff[1] = (int)((((int)file_stat.st_size) >> 16) & 0xFF);
+    buff[2] = (int)((((int)file_stat.st_size) >> 8) & 0XFF);
+    buff[3] = (int)((((int)file_stat.st_size) & 0XFF));
+
+    buff[4] = (int)((BUFFER_SIZE >> 24) & 0xFF);
+    buff[5] = (int)((BUFFER_SIZE >> 16) & 0xFF);
+    buff[6] = (int)((BUFFER_SIZE >> 8) & 0XFF);
+    buff[7] = (int)((BUFFER_SIZE & 0XFF));
+
+    /* Encrypt the header  */
+    ciphertext_len = entropy_encrypt (buff, HEADER_SIZE, ciphertext);
+    if(ciphertext_len <0)
+    {
+    	log_error("Error encrypting the AOF file size");
+    	goto error;
+    }
+    loga("Ciphertext Length is %d",ciphertext_len);
+    loga("sizeof ciphertext %d", sizeof(ciphertext));
+//    loga("unencrypted text: %02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", buff[0], buff[1], buff[2], buff[3],buff[4], buff[5], buff[6], buff[7]);
+//    loga("cyphertext: %02X:%02X:%02X:%02X", ciphertext[0], ciphertext[1], ciphertext[2], ciphertext[3]);
+
+
+    /* transmit the file size in encrypted or unencrypted format */
+    if(ENCRYPT_FLAG == 1)
+    	transmit_len = send(peer_socket, ciphertext, sizeof(ciphertext), 0);
+    else
+    	transmit_len = send(peer_socket, buff, sizeof(buff), 0);
+
+
+	if (transmit_len < 0)
+	{
+	    log_error("Error on sending AOF file size --> %s", strerror(errno));
+    	goto error;
+    }
+	else if (transmit_len > CIPHER_SIZE){
+		log_error("Transmit Length is longer than CIPHER SIZE --> "
+				"transmit: %d cipher size: %d", transmit_len, CIPHER_SIZE);
+    	 goto error;
+	}
+    loga("The size of the cipher text is %d",sizeof(ciphertext));
+
+
+	/* Determine the number of chunks
+	 * if the size of the file is larger than the Buffer size
+	 * then split it, otherwise we need one chunk only.
+	 *  */
+	if(file_stat.st_size > BUFFER_SIZE){
+		nchunk = (int)(ceil(file_stat.st_size/BUFFER_SIZE) + 1);
+	}
+	else{
+		nchunk = 1;
+	}
+
+    /* Last chunk size is calculated by subtracting from the total file size
+     * the size of each chunk excluding the last one.
+     */
+   	last_chunk_size = (long)(file_stat.st_size - (nchunk-1) * BUFFER_SIZE);
+
+	loga("AOF File size %d - Chunk Size %d - Number of chunks %d - last chunk size: %ld",
+			file_stat.st_size, BUFFER_SIZE, nchunk, last_chunk_size);
+
+    for(i=0; i<nchunk; i++){
+
+        /* clear buffer before using it */
+        memset(&buff[0], 0, sizeof(buff));
+        /* Read file data in chunks of BUFFER_SIZE bytes */
+        if(i<nchunk-1){
+        	nread = fread (buff, sizeof(char), BUFFER_SIZE, fp);
+            ciphertext_len = entropy_encrypt (buff, BUFFER_SIZE, ciphertext);
+        }
+        else{
+        	nread = fread (buff, sizeof(char), last_chunk_size, fp);
+            ciphertext_len = entropy_encrypt (buff, last_chunk_size, ciphertext);
+        }
+        /* checking for errors */
+    	if (nread < 0){
+    		 log_error("Error reading chunk of AOF file --> %s", strerror(errno));
+         	 goto error;
+    	}
+
+        /* transmit the chunk encrypted/unencrypted */
+        if(ENCRYPT_FLAG == 1){
+        	if(ciphertext_len <0){
+        		log_error("Error encrypting the AOF chunk --> %s", strerror(errno));
+            	 goto error;
+        	}
+        	transmit_len = send(peer_socket, ciphertext, sizeof(ciphertext), 0);
+        }
+        else{
+        	if(i<nchunk-1){
+        		transmit_len = send(peer_socket, buff, BUFFER_SIZE, 0);
+        	}
+        	else{
+        		transmit_len = send(peer_socket, buff, last_chunk_size, 0);
+        	}
+        }
+
+    	if (transmit_len < 0){
+    		 log_error("Error sending the AOF chunk --> %s", strerror(errno));
+         	 goto error;
+    	}
+
+    }
+
+	/* close the file descriptor */
+	fclose(fp);
+	close(peer_socket);
+    loga("Chunks transferred: %d ---> AOF transfer completed!", i);
+
+	/* Clean up */
+	EVP_cleanup();
+    ERR_free_strings();
+
+    return;
+
+error:
+	fclose(fp);
+	close(peer_socket);
+	loga("closing socket because of entropy error.");
+    return;
+
+}
+
+static void *
+entropy_loop(void *arg)
+{
+    event_loop_entropy(entropy_snd_callback, arg);
+    return NULL;
+}
+
+
+/*
+ * Function: (static) entropy_conn_start
+ * --------------------
+ * Checks if resources are available, and initializes the connection information.
+ * Loads the IV and creates a new thread to loop for the entropy receive.
+ *
+ *  returns: r_status for the status of opening of the new connection.
+ */
+
+static rstatus_t
+entropy_conn_start(struct entropy *cn)
+{
+    rstatus_t status;
+
+    THROW_STATUS(entropy_listen(cn));
+
+    status = pthread_create(&cn->tid, NULL, entropy_loop, cn);
+    if (status < 0) {
+        log_error("reconciliation thread for socket create failed: %s", strerror(status));
+        return DN_ERROR;
+    }
+
+    return DN_OK;
+}
+
+
+
+/*
+ * Function:  entropy_snd_init
+ * --------------------
+ * Initiates the data for the connection towards another cluster for reconciliation
+ *
+ *  returns: a entropy_conn structure with information about the connection
+ *           or NULL if a new thread cannot be picked up.
+ */
+
+struct entropy *entropy_snd_init(uint16_t entropy_port, char *entropy_ip, struct context *ctx)
+{
+
+    rstatus_t status;
+    struct entropy *cn;
+
+    cn = dn_alloc(sizeof(*cn));
+    if (cn == NULL) {
+        log_error("Cannot allocate snd entropy structure");
+        goto error;
+    }
+
+    /* Loading of key/iv happens only once by calling entropy_key_iv_load which is a util function.
+     * The same key/iv are reused in the entropy_rcv_init as well.
+     */
+    if(entropy_key_iv_load(ctx) == DN_ERROR){
+    	log_error("recon_key.pem or recon_iv.pem cannot be loaded properly");
+        goto error;
+    }
+    keyIVLoaded = 1;
+
+    cn->port = entropy_port;
+    string_set_raw(&cn->addr, entropy_ip);
+
+    cn->entropy_ts = (int64_t)time(NULL);
+    cn->tid = (pthread_t) -1; //Initialize thread id to -1
+    cn->sd = -1; // Initialize socket descriptor to -1
+
+    status = entropy_conn_start(cn);
+    if (status != DN_OK) {
+       goto error;
+    }
+
+    cn->ctx = ctx;
+    return cn;
+
+error:
+    entropy_conn_destroy(cn);
+    return NULL;
+}
---
+++ src/entropy/dyn_entropy_util.c
@@ -0,0 +1,317 @@
+/*
+ * Dynomite - A thin, distributed replication layer for multi non-distributed storages.
+ * Copyright (C) 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *_stats_pool_set_ts
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> // for open
+#include <unistd.h> //for close
+#include <math.h> // to do ceil for number of chunks
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/sendfile.h>
+#include <netinet/in.h>
+
+#include "dyn_core.h"
+
+
+/* A 128 bit key  */
+static unsigned char *theKey = (unsigned char *)"0123456789012345";
+
+/* A 128 bit IV  */
+static unsigned char *theIv = (unsigned char*)"0123456789012345";
+
+
+/*
+ * Function: entropy_decrypt
+ * --------------------
+ *  Decrypt the input data using the key and the Initialization Vector (IV).
+ *  Uses AES_256_CBC
+ *
+ *  returns: the length of the ciphertext if it has ended successfully,
+ *  or the DN_ERROR status.
+ *
+ */
+
+int entropy_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int plaintext_len = 0;
+
+  /* Create and initialize the context */
+  if(!(ctx = EVP_CIPHER_CTX_new()))
+	  return DN_ERROR;
+
+  /* Initialize the decryption operation with 256 bit AES */
+  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, theKey, theIv))
+	  return DN_ERROR;
+
+  /* Provide the message to be decrypted, and obtain the encrypted output.
+   * EVP_EncryptUpdate can be called multiple times if necessary
+   */
+  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+	  return DN_ERROR;
+  plaintext_len = len;
+
+  /* Finalize the decryption. Further ciphertext bytes may be written at
+   * this stage.
+   */
+  if(1 != EVP_DecryptFinal_ex(ctx, ciphertext + len, &len))
+	  return DN_ERROR;
+  plaintext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+  return plaintext_len;
+}
+
+
+/*
+ * Function: entropy_encrypt
+ * --------------------
+ *  Encrypts the input data using the key and the Initialization Vector (IV).
+ *  Uses AES_256_CBC
+ *
+ *  returns: the length of the ciphertext if it has ended successfully,
+ *  or the DN_ERROR status.
+ *
+ */
+
+int entropy_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *ciphertext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int ciphertext_len = 0;
+
+  /* Create and initialize the context */
+  if(!(ctx = EVP_CIPHER_CTX_new()))
+	  return DN_ERROR;
+
+  /* Padding */
+  if(1 != EVP_CIPHER_CTX_set_padding(ctx,1))
+	  return DN_ERROR;
+
+  /* Initialize the encryption operation with 256 bit AES */
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, theKey, theIv))
+	  return DN_ERROR;
+
+  /* Provide the message to be encrypted, and obtain the encrypted output.
+   * EVP_EncryptUpdate can be called multiple times if necessary
+   */
+  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+	  return DN_ERROR;
+  ciphertext_len = len;
+
+  /* Finalize the encryption. Further ciphertext bytes may be written at
+   * this stage.
+   */
+  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+	  return DN_ERROR;
+  ciphertext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+
+
+  return ciphertext_len;
+}
+
+
+/*
+ * Function: entropy_conn_stop
+ * --------------------
+ * closes the socket connection
+ */
+
+void
+entropy_conn_stop(struct entropy *cn)
+{
+    close(cn->sd);
+}
+
+/*
+ * Function:  entropy_conn_destroy
+ * --------------------
+ * Frees up the memory pointer for the connection
+ */
+
+void
+entropy_conn_destroy(struct entropy *cn)
+{
+	entropy_conn_stop(cn);
+    dn_free(cn);
+}
+
+/*
+ * Function:  entropy_listen
+ * --------------------
+ *  returns: r_status for the status of the new socket and
+ *  corresponding phases, e.g. socket, bind, listen etc.
+ */
+
+rstatus_t
+entropy_listen(struct entropy *cn)
+{
+    rstatus_t status;
+    struct sockinfo si;
+
+    status = dn_resolve(&cn->addr, cn->port, &si);
+    if (status < 0) {
+        return status;
+    }
+
+    cn->sd = socket(si.family, SOCK_STREAM, 0);
+    if (cn->sd < 0) {
+        log_error("reconciliation socket failed: %s", strerror(errno));
+        return DN_ERROR;
+    }
+
+    status = dn_set_reuseaddr(cn->sd);
+    if (status < 0) {
+        log_error("reconciliation set reuseaddr on m %d failed: %s", cn->sd, strerror(errno));
+        return DN_ERROR;
+    }
+
+    status = bind(cn->sd, (struct sockaddr *)&si.addr, si.addrlen);
+    if (status < 0) {
+        log_error(" reconciliation bind on m %d to addr '%.*s:%u' failed: %s", cn->sd,
+                  cn->addr.len, cn->addr.data, cn->port, strerror(errno));
+        return DN_ERROR;
+    }
+
+    status = listen(cn->sd, SOMAXCONN);
+    if (status < 0) {
+        log_error("reconciliation listen on m %d failed: %s", cn->sd, strerror(errno));
+        return DN_ERROR;
+    }
+
+
+    log_debug(LOG_NOTICE, "reconciliation m %d listening on '%.*s:%u'", cn->sd,
+    		cn->addr.len, cn->addr.data, cn->port);
+
+    return DN_OK;
+}
+
+/*
+ * Function:  entropy_iv_load
+ * --------------------
+ *
+ * Loads the send IV from a file
+ */
+rstatus_t
+entropy_key_iv_load(struct context *ctx){
+
+	int 			fd;
+    struct stat     file_stat;
+    unsigned char   buff[BUFFER_SIZE];
+
+    struct server_pool *pool = (struct server_pool *) array_get(&ctx->pool, 0);
+
+    /* 1. Check if the String array of the file names has been allocated */
+    if (string_empty(&pool->recon_key_file) || string_empty(&pool->recon_iv_file)) {
+    	log_error("Could NOT read key or iv file");
+    	return DN_ERROR;
+    }
+
+    /* 2. allocate char based on the length in the string arrays */
+    char key_file_name[pool->recon_key_file.len + 1];
+    char iv_file_name[pool->recon_iv_file.len + 1];
+
+    /* copy the content to the allocated array */
+    memcpy(key_file_name, pool->recon_key_file.data, pool->recon_key_file.len);
+    key_file_name[pool->recon_key_file.len] = '\0';
+    memcpy(iv_file_name, pool->recon_iv_file.data, pool->recon_iv_file.len);
+    iv_file_name[pool->recon_iv_file.len] = '\0';
+
+    loga("Key File name: %s - IV File name: %s", key_file_name, iv_file_name);
+
+    /* 3. checking if the key and iv files exist using access */
+    if( access(key_file_name, F_OK ) < 0 ) {
+    	log_error("Error: file %s does not exist", key_file_name);
+        return DN_ERROR;
+    }
+    else if( access(iv_file_name, F_OK ) < 0 ) {
+    	log_error("Error: file %s does not exist", iv_file_name);
+        return DN_ERROR;
+    }
+
+    /* 4. loading the .pem files */
+    FILE *key_file = fopen(key_file_name,"r");
+    if(key_file == NULL){
+    	    log_error("opening key.pem file failed %s", pool->recon_key_file);
+    	    return DN_ERROR;
+    }
+    FILE *iv_file = fopen(iv_file_name,"r");
+	if(iv_file == NULL){
+	    log_error("opening iv.pem file failed %s", pool->recon_iv_file);
+	    return DN_ERROR;
+	}
+
+	/* 5. using the file descriptor to do some checking with the BUFFER_SIZE */
+    fd = fileno(key_file);
+    if (fstat(fd, &file_stat) < 0)   					 /* Get the file size */
+    {
+        log_error("Error fstat --> %s", strerror(errno));
+    	return DN_ERROR;
+    }
+    else if (file_stat.st_size > BUFFER_SIZE){			/* Compare file size with BUFFER_SIZE */
+       	log_error("key file size is bigger then the buffer size");
+   	    return DN_ERROR;
+    }
+
+    fd = fileno(iv_file);
+    if (fstat(fd, &file_stat) < 0)
+    {
+    	log_error("Error fstat --> %s", strerror(errno));
+ 	    return DN_ERROR;
+    }
+    else if (file_stat.st_size > BUFFER_SIZE){
+    	log_error("IV file size is bigger then the buffer size");
+	    return DN_ERROR;
+    }
+
+    /* 6. reading the files for the key and iv */
+    if (fgets(buff,BUFFER_SIZE-1,key_file) == NULL){
+       	log_error("Processing Key file error");
+       	return DN_ERROR;
+    }
+  //  theKey = (unsigned char *)buff;
+    loga("key loaded: %s", theKey);
+
+    memset( buff, '\0', BUFFER_SIZE );
+    if (fgets(buff,BUFFER_SIZE-1,iv_file) == NULL){
+    	log_error("Processing IV file error");
+    	return DN_ERROR;
+    }
+   // theIv = (unsigned char *)buff;
+    loga("iv loaded: %s", theIv);
+
+    return DN_OK;
+}
+
