e0eaad87d875276419c95791513ef1559d8723d6
ipapapa <iduckhd@hotmail.com>
2016-01-22 10:26:40 -0800

    super fast logo

--- src/dyn_asciilogo.h
+++ src/dyn_asciilogo.h
@@ -2,18 +2,14 @@
  * Dynomite - A thin, distributed replication layer for multi non-distributed storages.
  * Copyright (C) 2015 Netflix, Inc.
  *
- * author: Ioannis Papapanagiotou
  */ 
 
 
 char *ascii_logo =
-"                                                                      \n"                                                       
-"     #                                \      m                        \n"
-"  mmm#  m   m  mmmm    mmm   mmmmm  mmm    mm#mm   mmm                \n"  
-" #   #  \\m m/  #   #  #   #  # # #    #      #    #   #               \n"
-" #   #   #m#   #   #  #   #  # # #    #      #    #''''               \n"
-" \\#m##   \\#    #   #   #m#   # # #  mm#mm    mm    #mm                \n"
-"         m/                                                           \n"
-"        ##                                                            \n"
-"                                                                      \n\n";
-
+"\n\n"
+"  _________                                __________      \n"
+"  ____  __ \\____  _____________________ ______(_)_  /_____  \n"
+"  ___  / / /_  / / /_  __ \\  __ \\_  __ `__ \\_  /_  __/  _ \ \n"
+"  __  /_/ /_  /_/ /_  / / / /_/ /  / / / / /  / / /_ /  __/ \n"
+"   /_____/ _\\__, / /_/ /_/\\____//_/ /_/ /_//_/  \\__/ \\___/ \n"
+"           /____/   \n\n";
